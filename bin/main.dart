bf146df6ae312aefd7fb8ed664cb43f467df8422
McLarenCollege <hello@mclarencollege.in>
2019-10-22 08:28:21 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,27 @@
+// Challenge 1
+// Write failing tests for the next two challenges
+
+// Challenge 2
+// Matrix Transpose
+// Create a function that transposes a 2D matrix.
+// transposeMatrix([
+//  [1, 1, 1],
+//  [2, 2, 2],
+//  [3, 3, 3]
+//    ]) ➞ [
+//    [1, 2, 3],
+//    [1, 2, 3],
+//    [1, 2, 3]
+//  ]
+
+
+
+// Challenge 3
+// Sieve of Eratosthenes
+// Given N as input, return an array with all primes up to N included.
+//  Examples
+//  eratosthenes(10) ➞ [2, 3, 5, 7]
+//  eratosthenes(20) ➞ [2, 3, 5, 7, 11, 13, 17, 19]
+
+main() {
+}
